917e2d091e007ca19d10e3bae04b746af915c93d
Sharangdhar Shree <sharangdhar7984@gmail.com>
2025-08-10 02:10:41 +0530

    added db seed values, completed listSchools controller

--- src/controllers/school.controller.js
+++ src/controllers/school.controller.js
@@ -1,4 +1,5 @@
 import School from "../models/school.model.js";
+import { getDistance } from "geolib";
 import { z } from "zod/v4";
 
 // zod input validation schema
@@ -8,7 +9,12 @@ const schoolSchema = z.object({
   latitude: z.float32(),
   longitude: z.float32(),
 });
+const coordinateSchema = z.object({
+  latitude: z.float32(),
+  longitude: z.float32(),
+});
 
+// controllers
 const homePage = async (req, res) => {
   res.send("Hello There!! This is Home Page.");
 };
@@ -41,9 +47,33 @@ Method: GET
 Parameters: User's latitude and longitude.
 Functionality: Fetches all schools from the database, sorts them based on proximity to the user's location, and returns the sorted list.
 Sorting Mechanism: Calculate and sort by the geographical distance between the user's coordinates and each school's coordinates.*/
-  res.send("fetching list of all schools....");
+
   try {
-  } catch (error) {}
+    const payload = coordinateSchema.parse(req.body);
+
+    const schools = await School.findAll();
+    if (!schools) {
+      throw error.statusCode(401).message("school entry fetch failed!!");
+    }
+
+    // sort on the basis of their geo-distance from given co-ordinates
+    // use geolib to calculate distance between each schools and given co-ordinate
+    // and finally sort then on the basis of the distance
+    const sorted = schools
+      .map((school) => ({
+        ...school.get({ plain: true }),
+        distance: getDistance(
+          { latitude: payload.latitude, longitude: payload.longitude },
+          { latitude: school.latitude, longitude: school.longitude }
+        ), // in meters
+      }))
+      .sort((a, b) => a.distance - b.distance);
+
+    res.status(201).json(sorted);
+  } catch (error) {
+    console.log("something went wrong while fetching schools!!");
+    res.send(error);
+  }
 };
 
 export { homePage, addSchool, listSchools };
