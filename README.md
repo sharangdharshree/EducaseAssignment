3f4d0b4e4d2f38ea06dc264ccb2867a9a9528b9a
Sharangdhar Shree <sharangdhar7984@gmail.com>
2025-08-10 02:19:52 +0530

    cors setup

---
+++ README.md
@@ -0,0 +1,4 @@
+points to remember:
+configure cors when deploying --> DONE
+initiate deploy on render
+c onfigure aws rds ip for deployed server
--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 
 app.use(express.json());
 
